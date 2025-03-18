6705b9deb14c00036ff2e2c0597b635ba7564d98
unknown <isomiddinmamadaminov133@gmail.com>
2025-03-18 15:38:33 +0500

    products have been corrected

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\HasDatabaseNotifications;
-
 class Product extends Model
 {
     use HasFactory;
